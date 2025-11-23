89434e559889be1aa51ac785d5c912979ddadaa1
Kim Bum Soo <bumsookim333@naver.com>
2025-11-23 15:23:37 +0900

    init commit

---
+++ src/main/java/kr/devport/backend/DevportApiApplication.java
@@ -0,0 +1,13 @@
+package kr.devport.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevportApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevportApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/devport/backend/DevportApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.devport.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevportApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
